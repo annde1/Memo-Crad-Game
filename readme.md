e51ab4e3a764f8e229482b05ad4e97c49473b300
annde1 <annadepta61@gmail.com>
2023-09-11 11:48:52 +0300

    Final 🥳

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Memory Card Game (Card Flip Challenge)
+
+## Description
+
+**Card Flip Challenge** is a classic memory card game that challenges your memory and concentration. It's a fun and engaging way to test your cognitive skills while enjoying beautiful card designs.
+
+## Features
+
+- **Memory Challenge:** Test your memory by matching pairs of cards.
+- **Beautiful Designs:** Enjoy visually appealing card designs.
+- **Customizable:** Easily customize the game by adding your own card images.
+- **Responsive:** Play the game on any device with a responsive design.
+
+### How to play
+
+- Click on any card to reveal its image.
+- Click on another card to reveal its image.
+- If the two cards match, they will remain face up.
+- If the two cards do not match, they will flip back face down.
+- Continue matching pairs of cards until all pairs are matched to win the game.
+
+### Game Rules
+
+- The game is won when all pairs of cards are matched.
+- You have a limited number of attempts to complete the game.
+- The game keeps track of your score and time to complete.
--- script/app.js
+++ script/app.js
@@ -3,35 +3,46 @@ const playerLives = document.querySelector("#player-lives");
 const sound = document.querySelector("#flip-sound");
 
 //Functions:
+
+// Function to reset the game to its initial state
 const resetGame = () => {
   const message = document.querySelector("#message");
   let randomCards = game.randomCards();
   const faces = document.querySelectorAll(".face");
   const cards = document.querySelectorAll(".theCard");
+  // Loop through each card and reset their state
   randomCards.forEach((randomCard, index) => {
     cards[index].classList.remove("transform");
     //Wait for cards to flip back and randomize
     setTimeout(() => {
+      // Enable clicking on the cards
       cards[index].style.pointerEvents = "all";
+      // Set card name and face image
       cards[index].setAttribute("name", randomCard.name);
       faces[index].src = randomCard.src;
     }, 1000);
   });
+  // Reset player's lives and update the display
   player.lives = 8;
   playerLives.innerHTML = `Score: ${player.lives}`;
   message.innerHTML = "";
 };
 
+// Function to check if the player has won the game
 const checkIfWon = () => {
+  // Get all transformed cards
   const cards = document.querySelectorAll(".transform");
+  // If the number of transformed cards equals the total number of cards, the player has won
   if (cards.length === game.cards.length) {
     return true;
   }
 };
 
+// Class for the game logic
 class Game {
   constructor(element) {
     this.gameContainer = document.querySelector(`#${element}`);
+    // An array of card objects with src and name properties
     this.cards = [
       { src: "assets/images/maki.png", name: "maki" },
       { src: "assets/images/nigiri.png", name: "nigiri" },
@@ -57,21 +68,26 @@ class Game {
   }
   //method randomCards sorts the array of cards randomly
   randomCards() {
+    // Randomly shuffle the cards using the Fisher-Yates algorithm
     let random = this.cards.sort(() => Math.random() - 0.5);
     return random;
   }
-  //Methid generateCards randomly generates html for the cards
+  //Method generateCards generates html for the cards
   generateCard() {
+    // Generate a random order of cards
     let items = this.randomCards();
+    // Loop through each card and create HTML elements for them
     for (const item of items) {
       const card = document.createElement("div");
       const face = document.createElement("img");
       const back = document.createElement("div");
+      // Set classes and attributes for card elements
       card.classList = "theCard";
       card.setAttribute("name", item.name);
       face.classList = "face";
       face.src = item.src;
       back.classList = "back";
+      // Append card elements to the game container
       this.gameContainer.appendChild(card);
       card.appendChild(face);
       card.appendChild(back);
@@ -79,6 +95,7 @@ class Game {
   }
   //Method checkCards checks if the two selecte cards are matching (returns true or false)
   checkCards(cards) {
+    // Compare the 'name' attribute of the two cards to determine if they match
     if (cards[0].getAttribute("name") === cards[1].getAttribute("name")) {
       return true;
     } else {
@@ -87,24 +104,30 @@ class Game {
   }
 }
 
+// Class for the player
 class Player {
   constructor(lives) {
     this.lives = lives;
   }
+  // Method to decrease the player's lives
   decreaseLive() {
     this.lives--;
   }
+  // Method to check if the player is out of lives
   isOutOfLives() {
+    // If the player has no lives left, return true
     if (this.lives === 0) {
       return true;
     }
   }
 }
+// Initialize a player with 8 lives
 let player = new Player(8);
-
+// Initialize the game with the game container element
 let game = new Game("game-container");
 game.generateCard();
 
+// Event listener for card clicks within the game container
 game.gameContainer.addEventListener("click", (e) => {
   //Getting the element from which generated the event (event delegation to the parent container)
   const card = e.target.closest(".theCard");
